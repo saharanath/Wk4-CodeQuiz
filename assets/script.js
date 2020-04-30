7c7928090b99d1938bf50a1a85e65df0ad7ee792
Sahara Nath <saharanath@hotmail.com>
2020-04-29 18:06:17 -0700

    highScore function, readMe, comments

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,16 @@ var option3 = document.getElementById("option3");
 var intials = document.getElementById("user-initials");
 var buttonEl = document.getElementById("start-game");
 var elements = document.getElementsByClassName("clickOption");
+var scoreboard = document.getElementById("intial-form")
+//timer
 var totalTime = 75;
+//current question
 var questionNum = 0;
 
+//hide score board at start
+scoreboard.style.visibility = "hidden";
+
+//array of questions and the answer options for each
 var questions =["The condition in an if/else statement is enclosed with in _______",
                 "Arrays in JavaScript can be used to store ______",
                 "Commonly used data types do not include ______",
@@ -24,22 +31,34 @@ var answers =[question1= ["Quotes","Curly brackets","Parentheses","Square bracke
             
 var correctAnswers = [2,3,2,2];
 
+//start game with button, start timer, display first question
+start.addEventListener("click", function(){
+    timer();
+    displayQuestion(questionNum);
+    start.style.visibility = "hidden";
+    buttonEl.style.visibility = "visible";
+
+});
+
+//validates which answer choice is selected
 var clickOption = function(){
 
+    //stores answer as integer to match array
     var selectedOption = parseInt(this.getAttribute("data-index"));
-
+    
+    //correct answer
     if (selectedOption === correctAnswers[questionNum]){
         alert("correct"); 
     }
-    
+    //wrong answer- 5 sec off timer
     else{
         totalTime -= 5;
     }
     
+    //move to next question
     questionNum += 1;
     
     if(questionNum >= questions.length){
-    
         return;
     }
 
@@ -51,20 +70,14 @@ for(var i = 0; i < elements.length; i++){
     elements[i].addEventListener("click", clickOption);
 }
 
-start.addEventListener("click", function(){
-    timer();
-    displayQuestion(questionNum);
-    start.style.visibility = "hidden";
-    buttonEl.style.visibility = "visible";
-});
-
 function timer(){
-    
+
+    //timer counts down by seconds stops and ends game if out of time or questions
     var timerInterval = setInterval(function(){
         totalTime --;
         timerEl.textContent = totalTime;
  
-        if(totalTime === 0){
+        if(totalTime === 0 || questionNum >= questions.length){
         clearInterval(timerInterval);
         endQuiz();
         return;
@@ -79,4 +92,28 @@ function displayQuestion(index){
     option2.textContent=(answers[index][2]);
     option3.textContent=(answers[index][3]);
 }
+
+function endQuiz(){
+    questionEl.textContent = ("Thanks for playing");
+    buttonEl.style.visibility="hidden";
+    scoreboard.style.visibility="visible";
+}
+ 
+//prompts user to enter their intials and saves with their score
+function highScore(){
+     var inputInitials = document.getElementById("intials").addEventListener("submit",function(event){
+        event.preventDefault();
+        var initials = [];
+        var scores =[];
+        localStorage.setItem("initials", inputInitials.value);
+        scores.push(totalTime);
+        initials.push(inputInitials);
+        console.log(initials);
+        console.log(scores);
+     });
+
+
+     
+ }
+
 
